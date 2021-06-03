4ee72619871e2907fe9955cf5e2c8be7599e5163
GitHubSoham <81175521+GitHubSoham@users.noreply.github.com>
2021-06-03 16:24:16 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,29 @@
+class Stone{
+
+constructor(x,y){
+
+var options = {
+    isStatic: false,
+    restitution:0.8,
+    friction:0.9,
+    density:15
+
+}
+this.body = Bodies.rectangle(x,y,20,50,options);
+this.width = 20;
+this.height = 50;
+World.add(world, this.body);
+}
+
+display(){
+var pos =this.body.position;
+var angle = this.body.angle;
+push();
+translate(pos.x,pos.y)
+rotate(angle);
+rectMode(CENTER);
+fill("red");
+rect(0, 0, this.width, this.height);
+pop();
+}
+};
